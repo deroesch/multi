d3210b7655403752f02840e6a7dd9950df229126
Douglas Roesch <Douglas.Roesch@bnymellon.com>
2020-12-12 12:56:05 -0500

    monochrome = true

--- mult/src/test/java/bdd/RunCucumberTest.java
+++ mult/src/test/java/bdd/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/bdd/features")
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/bdd/features", monochrome = true)
 public class RunCucumberTest {
 
 }
