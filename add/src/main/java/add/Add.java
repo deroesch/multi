2ff3ec0895a9018e693062fa3390bf980fe1f6a0
Douglas Roesch <Douglas.Roesch@bnymellon.com>
2020-12-12 11:34:04 -0500

    Added javadocs

--- add/src/main/java/add/Add.java
+++ add/src/main/java/add/Add.java
@@ -1,6 +1,19 @@
 package add;
 
+/**
+ * A simple class for adding long integers
+ * 
+ * @author doug.roesch@gmail.com
+ */
 public class Add {
+
+	/**
+	 * Returns the sum of two addends
+	 * 
+	 * @param a the left addend
+	 * @param b the right addend
+	 * @return the sum of a + b
+	 */
 
 	public long add(long a, long b) {
 		return a + b;
--- expo/src/main/java/expo/Expo.java
+++ expo/src/main/java/expo/Expo.java
@@ -1,7 +1,19 @@
 package expo;
 
+/**
+ * A simple class for performing exponentiation on long integers
+ * 
+ * @author doug.roesch@gmail.com
+ */
 public class Expo {
 
+	/**
+	 * Returns the product of a base integer and its exponent
+	 * 
+	 * @param a the base integer
+	 * @param b the exponent
+	 * @return the result of a ** b
+	 */
 	public double expo(long a, long b) {
 		return Math.pow(a, b);
 	}
